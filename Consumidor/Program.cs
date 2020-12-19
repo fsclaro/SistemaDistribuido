2be640627ef17ce3782de423e3755a9b3582672f
Fernando Salles Claro <fernando@MacBook.local>
2020-12-18 21:57:44 -0300

    Commit inicial

---
+++ Consumidor/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+
+namespace Consumidor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceBusClient = new SubscriptionClient(
+                    "Endpoint=sb://aceleracaoavanade.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=pa7LRhiDmi7ik4m812/M8CebQ6rBFJPCe4ZqduNhOm0=",
+                    "pagamentofeito",
+                    "PagamentoFeitoServicoB"
+                );
+
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+
+            Console.WriteLine("Consumidor - Recebendo a mensagem...");
+            serviceBusClient.RegisterMessageHandler(ProcessMessageAsync, messageHandlerOptions);
+
+            while (true) { }
+        }
+
+
+
+
+
+        private static Task ProcessMessageAsync(Message message, CancellationToken arg2)
+        {
+            var pagamentoFeito = message.Body.ParseJson<PagamentoFeito>();
+
+            Console.WriteLine(pagamentoFeito.ToString());
+
+            return Task.CompletedTask;
+        }
+
+
+
+
+
+        private static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+
+
+    internal class PagamentoFeito
+    {
+        public string NumeroCartao { get; set; }
+        public decimal Valor { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"Cartao {NumeroCartao}, Valor {Valor}";
+            ;
+        }
+    }
+
+
+
+
+    public static class Utils
+    {
+        private static readonly UTF8Encoding Utf8NoBom = new UTF8Encoding(false);
+
+        private static readonly JsonSerializerSettings JsonSettings = new JsonSerializerSettings
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            DateFormatHandling = DateFormatHandling.IsoDateFormat,
+            DefaultValueHandling = DefaultValueHandling.Ignore,
+            MissingMemberHandling = MissingMemberHandling.Ignore,
+            ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+            TypeNameHandling = TypeNameHandling.None,
+            Converters = new JsonConverter[] { new StringEnumConverter() }
+        };
+
+
+
+        public static T ParseJson<T>(this byte[] json)
+        {
+            if (json == null || json.Length == 0) return default;
+
+            var result = JsonConvert.DeserializeObject<T>(Utf8NoBom.GetString(json), JsonSettings);
+
+            return result;
+        }
+    }
+}
---
+++ Produtor/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Text;
+using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+
+namespace Produtor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // definindo a mensagem
+            var pagamentofeito = new PagamentoFeito { NumeroCartao = "123456789012", Valor = 1502.32M };
+
+            // definindo o service bus
+            var serviceBusClient = new TopicClient(
+                "Endpoint=sb://aceleracaoavanade.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=pa7LRhiDmi7ik4m812/M8CebQ6rBFJPCe4ZqduNhOm0=",
+                "pagamentofeito");
+
+            // define uma mensagem
+            var message = new Message(pagamentofeito.ToJsonBytes());
+            message.ContentType = "application/json";
+            message.UserProperties.Add("CorrelationId", Guid.NewGuid().ToString());
+
+            while (true)
+            {
+                Console.ReadKey();
+
+                Console.WriteLine("Produtor - Enviando a mensagem...");
+
+                // envia a mensagem
+                serviceBusClient.SendAsync(message);
+            }
+
+            // aguarda o usuário dar Enter
+            Console.ReadKey();
+            Console.WriteLine("Produtor - Encerrando.");
+        }
+    }
+
+
+    internal class PagamentoFeito
+    {
+        public string NumeroCartao { get; set; }
+        public decimal Valor { get; set; }
+    }
+
+    public static class Utils
+    {
+        private static readonly UTF8Encoding Utf8NoBom = new UTF8Encoding(false);
+
+        private static readonly JsonSerializerSettings JsonSettings = new JsonSerializerSettings
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            DateFormatHandling = DateFormatHandling.IsoDateFormat,
+            DefaultValueHandling = DefaultValueHandling.Ignore,
+            MissingMemberHandling = MissingMemberHandling.Ignore,
+            ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+            TypeNameHandling = TypeNameHandling.None,
+            Converters = new JsonConverter[] { new StringEnumConverter() }
+        };
+
+
+
+        /// <summary>
+        /// Converts the object to json bytes.
+        /// </summary>
+        /// <param name="source">The source</param>
+        /// <returns></returns>
+        public static byte[] ToJsonBytes(this object source)
+        {
+            if (source == null)
+                return null;
+
+            var instring = JsonConvert.SerializeObject(source, Formatting.Indented, JsonSettings);
+            return Utf8NoBom.GetBytes(instring);
+        }
+
+    }
+
+}
+
